42ff0783661b7a8e7936d57c61eb433185d51ca6
vpascal <vpascal2@gmail.com>
2024-12-20 22:33:02 -0500

    progress_bar_added

--- perform_ocr.py
+++ perform_ocr.py
@@ -5,21 +5,24 @@ from dotenv import load_dotenv
 from src.ocr_multiprocessing import ocr
 from src.helpers import create_directory
 import time
+from tqdm import tqdm
 
 
 load_dotenv()
 
-uga_image_path = Path(os.getenv("IMAGE_PATH_UGA"))
+uga_image_path = Path(os.getenv("IMAGE_PATH_STUREC"))
 uga_image_output_path = Path(uga_image_path,'json_output')
 
 files = list(uga_image_path.glob('*'))
 os.environ['OMP_THREAD_LIMIT'] = '1'
 
 def main():
-    with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
-        for i in zip(files,executor.map(ocr,files)):
-            print('processed')
- 
+    with tqdm(total=len(files)) as progress:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
+            for i in zip(files,executor.map(ocr,files)):
+                progress.update()
+
+
 if __name__ == '__main__':
     create_directory(uga_image_output_path)
     start = time.time()
